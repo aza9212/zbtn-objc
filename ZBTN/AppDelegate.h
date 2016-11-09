c3cb889a10af747860631c6b2b999cfcb1c7bc60
Azamat Kushmanov <azamat.kushmanov@gmail.com>
2016-11-09 23:25:11 +0600

    Initial Commit

---
+++ ZBTN/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZBTN
+//
+//  Created by Azamat Kushmanov on 11/9/16.
+//  Copyright © 2016 Azamat Kushmanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZBTN/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZBTN
+//
+//  Created by Azamat Kushmanov on 11/9/16.
+//  Copyright © 2016 Azamat Kushmanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
